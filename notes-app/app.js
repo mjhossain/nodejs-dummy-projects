17fb3f0c10f281127c4171d0c1735e371c35583f
mjhossain <mjhossainnyc@gmail.com>
2020-06-04 20:51:26 -0400

    updates

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,45 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const notes = require('./notes.js');
+const yargs = require('yargs');
 
-console.log(chalk.bold.green.inverse('Success!'));
+//console.log(chalk.bold.green.inverse('Success!'));
+// console.log(process.argv);
+// console.log(yargs.argv);
 
-console.log(process.argv);
+//console.log('hello')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    handler: function() {
+        console.log('Adding a note..')
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note..')
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('Listing all notes..')
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note..')
+    }
+});
+
+
+console.log(yargs.argv);
