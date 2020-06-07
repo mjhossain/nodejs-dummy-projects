df7009e9d48e2a18b376b3f0c0566cc61a73b0cc
mjhossain <mjhossainnyc@gmail.com>
2020-06-06 21:34:52 -0400

    updates

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,34 @@
 const apiKey = require('./api-key')
 const request = require('postman-request')
 
-const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=New%20York`;
+const weatherUrl = `http://api.weatherstack.com/current?access_key=${apiKey.weatherAPI}&query=New%20York`;
 
-request(url, (error, response, body) => {
-    console.log('Status Code: ', response.statusCode)
-    const data = JSON.parse(body);
-    console.log(data.current)
+
+// request({ url: weatherUrl, json: true }, (error, response, body) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!')
+//     } else if (body.error) {
+//         console.log('Unable to find location!')
+//     } else {
+//         const curWeather = body.current;
+//         console.log(`It's currently ${curWeather.temperature} degrees out with ${curWeather.precip * 100}% chance of rain.`);
+//     }
+
+// })
+
+
+// Geo Location Request
+
+const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${apiKey.addrAPI}&limit=1`;
+
+request({ url: geoUrl, json: true }, (error, response, body) => {
+    if (response.statusCode == 200) {
+        const latitude = body.features[0].center[1]
+        const longitude = body.features[0].center[0]
+        console.log(`Longitude: ${longitude}\nLatitude: ${latitude}`)
+    } else if (error) {
+        console.log('Unable to connect to map service!')
+    } else {
+        console.log('Unable to fetch address!')
+    }
 })
