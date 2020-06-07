a8c8170ca317922a634f0fa6d97e357f0a18ce9c
mjhossain <mjhossainnyc@gmail.com>
2020-06-06 23:05:14 -0400

    updates

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,15 +4,19 @@ const forecast = require('./utils/forecast')
 
 const location = process.argv[2]
 
-geocode(location, (err, data) => {
-    if (err) {
-        return console.log(err)
-    }
-    forecast(data.lon, data.lat, (err, forecaseData) => {
+if (location != undefined) {
+    geocode(location, (err, data) => {
         if (err) {
             return console.log(err)
         }
-        console.log(`It is ${forecaseData.desc} with a tempreature of ${forecaseData.temp} degrees out. There is ${forecaseData.rain}% chance of rain.\n${forecaseData.loc}`)
-    })
+        forecast(data.lon, data.lat, (err, forecaseData) => {
+            if (err) {
+                return console.log(err)
+            }
+            console.log(`It is ${forecaseData.desc} with a tempreature of ${forecaseData.temp} degrees out. There is ${forecaseData.rain}% chance of rain.\n${forecaseData.loc}`)
+        })
 
-})
+    })
+} else {
+    console.log('\nUsage:\n\nnode app Miami \t\tOR\tnode app "89 E 42nd St, New York"\n')
+}
