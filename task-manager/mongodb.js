99fb0d382a71aa216c6aed1c7e7d3a3fd8e8c920
mjhossain <mjhossainnyc@gmail.com>
2020-06-16 16:15:56 -0400

    updates

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,12 +2,17 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
-const dbName = 'tash-manager'
+const dbName = 'task-manager'
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (err, client) => {
     if (err) {
         return console.log('Connection error!')
     }
 
-    console.log('DB connection successful!')
+    const db = client.db(dbName)
+
+    db.collection('users').insertOne({
+        name: 'Mohammed',
+        age: 23
+    })
 })
