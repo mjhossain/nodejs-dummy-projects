3e59480642bce797ea9044507dd19f797100b695
mjhossain <mjhossainnyc@gmail.com>
2020-06-18 22:20:18 -0400

    updates

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,13 +9,30 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email!')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
     },
     gender: {
-        type: String
+        type: String,
+        trim: true,
+        lowercase: true,
+        default: 'Unassigned'
     }
 })
 
@@ -27,27 +46,28 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const task = new Task({
-    description: 'Take out the garbage',
-    completed: false
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((err) => {
-    console.log(err)
-})
-
-
-// const user1 = new User({
-//     name: 'Zakir',
-//     age: 'Something',
-//     gender: 'Male'
+// const task = new Task({
+//     description: 'Take out the garbage',
+//     completed: false
 // })
 
-
-// user1.save().then(() => {
-//     console.log(user1)
+// task.save().then(() => {
+//     console.log(task)
 // }).catch((err) => {
-//     console.log('Error!', err)
+//     console.log(err)
 // })
+
+
+const user1 = new User({
+    name: 'Zakir    ',
+    email: '       moham@gmail.com   ',
+    age: 23,
+    gender: 'Male'
+})
+
+
+user1.save().then(() => {
+    console.log(user1)
+}).catch((err) => {
+    console.log('Error!', err)
+})
