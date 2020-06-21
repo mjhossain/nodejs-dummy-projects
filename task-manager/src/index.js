af86a0da17558611a24c5ba7ebbd21cec894237c
mjhossain <mjhossainnyc@gmail.com>
2020-06-21 01:36:25 -0400

    updates

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 // Connecting to MongoDB
 require('./db/mongoose')
@@ -6,19 +7,29 @@ require('./db/mongoose')
 // Importing Routers
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const testRouter = require('./routers/test')
+const { urlencoded } = require('express')
 
 // Creating Express Server
 const app = express()
 
 // Declaring Port
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3003
+
+
+// React setup
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 // Using express parser
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // Setting up Routers
 app.use(userRouter)
 app.use(taskRouter)
+app.use(testRouter)
 
 
 // Running App
