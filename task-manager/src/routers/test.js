2249a129ef94a2de181112adbd354e55bc0dda9f
mjhossain <mjhossainnyc@gmail.com>
2020-06-28 23:51:15 -0400

    updates

--- task-manager/src/routers/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-router.get('/test', (req, res) => {
-    res.send('This is working!')
-})
-
-module.exports = router
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,3 +1,21 @@
-test('Hello', () => {
+const math = require('../src/math')
 
+test('Tip Test : 12.5', () => {
+    const total = math.calculateTip(10)
+    expect(total).toBe(12)
+})
+
+test('Tip Test : 13', () => {
+    const total = math.calculateTip(10, .3)
+    expect(total).toBe(13)
+})
+
+test('Should convert 32 - 0', () => {
+    const temp = math.fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('Should convert 0 - 32', () => {
+    const temp = math.celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
 })
